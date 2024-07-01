20df63002111b38e93f002a0fe783257d931f046
WeiKuoLi <wli22@nd.edu>
2024-07-01 16:12:07 -0400

    bgfx

--- setup.sh
+++ setup.sh
@@ -20,8 +20,8 @@ echo 'alias sshxxx="ssh -XY wli22@bastion.crc.nd.edu"' >> ~/.bash_aliases
 
 ######## chrome #######
 # Install Google Chrome stable
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 ######## miniconda with jupyter notebook #######
 
